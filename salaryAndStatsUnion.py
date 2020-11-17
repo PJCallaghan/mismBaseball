efb84543936a7946e01057728ab4887b2763dccb
josephcallaghan <callaghan.j@husky.neu.edu>
2020-11-16 20:11:48 -0500

    Drop players who have not played a game and therefore do not have stats

--- salaryAndStatsUnion.py
+++ salaryAndStatsUnion.py
@@ -3,6 +3,6 @@ df = pd.read_csv("data/baseballreference-stats.csv")
 df.drop(columns=['Rk'],inplace=True)
 fan_graph_df = pd.read_csv("data/FanGraphs Leaderboard.csv")
 df = df.join(fan_graph_df.set_index('Name'), on='Name',lsuffix="br",rsuffix="salaries")
-# df.dropna(inplace=True)
+df.dropna(inplace=True)
 df.to_csv("data/salaries-and-fan-graph-stats.csv")
 
