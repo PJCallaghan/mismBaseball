afa0df58006ff681253cee87980fc366baf3f57f
josephcallaghan <callaghan.j@husky.neu.edu>
2020-11-16 20:01:53 -0500

    Advanced stats

--- salaryAndStatsUnion.py
+++ salaryAndStatsUnion.py
@@ -1,7 +1,8 @@
 import pandas as pd
 df = pd.read_csv("data/baseballreference-stats.csv")
 df.drop(columns=['Rk'],inplace=True)
-salary_df = pd.read_csv("data/Salaries.csv")
-df = df.join(salary_df.set_index('Name'), on='Name',lsuffix="br",rsuffix="salaries")
-df.dropna(inplace=True)
-df.to_csv("data/salaries-and-stats.csv")
+fan_graph_df = pd.read_csv("data/FanGraphs Leaderboard.csv")
+df = df.join(fan_graph_df.set_index('Name'), on='Name',lsuffix="br",rsuffix="salaries")
+# df.dropna(inplace=True)
+df.to_csv("data/salaries-and-fan-graph-stats.csv")
+
