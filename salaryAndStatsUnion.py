5869ef06af542709d3d2e2c8ed7a13d7f5c29488
josephcallaghan <callaghan.j@husky.neu.edu>
2020-11-16 20:30:11 -0500

    Drop usless data

--- salaryAndStatsUnion.py
+++ salaryAndStatsUnion.py
@@ -1,8 +1,10 @@
 import pandas as pd
-df = pd.read_csv("data/baseballreference-stats.csv")
-df.drop(columns=['Rk'],inplace=True)
+
+df = pd.read_csv("data/Salaries.csv")
 fan_graph_df = pd.read_csv("data/FanGraphs Leaderboard.csv")
-df = df.join(fan_graph_df.set_index('Name'), on='Name',lsuffix="br",rsuffix="salaries")
+fan_graph_df.drop(columns=["Team", "playerid"], inplace=True)
+df = df.join(fan_graph_df.set_index('Name'), on='Name', lsuffix="_salries", rsuffix="_fan_graphs")
 df.dropna(inplace=True)
 df.to_csv("data/salaries-and-fan-graph-stats.csv")
 
+# TODO: Remove duplicate stats
