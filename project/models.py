20b3ceac467dfc5cb3a7be5c4d0c39491b645719
josephcallaghan <callaghan.j@husky.neu.edu>
2020-11-19 14:21:55 -0500

    Fix models to use regressors not classifiers

--- project/models.py
+++ project/models.py
@@ -1,24 +1,19 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, f1_score
 from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.feature_selection import SelectKBest, f_regression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
 
 # %% Part 1
 df = pd.read_csv("../data/salaries-and-fan-graph-stats.csv")
-df.drop(["Name", "Unnamed: 0", "Team"], axis=1, inplace=True)
-df["BB%"].replace({r'%': ''}, inplace=True, regex=True)
-df["K%"].replace({r'%': ''}, inplace=True, regex=True)
-df["BB%"] = df["BB%"].astype(float)
-df["K%"] = df["K%"].astype(float)
+df.drop(["Name", "Team"], axis=1, inplace=True)
 df = pd.get_dummies(drop_first=True, data=df)
 y = df["Salary"]
 x = df.drop(["Salary"], axis=1)
-bestFeatures = SelectKBest(score_func=f_regression, k=4)
+bestFeatures = SelectKBest(score_func=f_regression, k=5)
 best_x = bestFeatures.fit_transform(x, y)
 x_train, x_test, y_train, y_test = train_test_split(best_x, y, test_size=0.3, random_state=1)
 
@@ -51,40 +46,22 @@ linear_y_pred = model.predict(x_test)
 l_mse = mean_squared_error(y_test, linear_y_pred)
 l_rmse = l_mse ** 0.5
 
-# %% Logistic Regression
-lr = LogisticRegression(solver="liblinear")
-lr.fit(x_train, y_train)
-lr_y_pred = lr.predict(x_test)
-lr_f1 = f1_score(y_test, lr_y_pred, average="weighted")
-print(f"Lgistic Regression f1 score is: {lr_f1}")
 
-# %% Decision tree
-dt = DecisionTreeClassifier()
-dt.fit(x_train, y_train)
-dt_y_pred = dt.predict(x_test)
-dt_f1 = f1_score(y_test, dt_y_pred, average="weighted")
-print(f"Decision Tree f1 score is: {dt_f1}")
 
-# %% Random Forest
-rfr = RandomForestClassifier(n_estimators=500)
-rfr.fit(x_train, y_train)
-rfr_y_pred = rfr.predict(x_test)
-rfr_f1 = f1_score(y_test, rfr_y_pred, average="weighted")
-print(f"Random Forest f1 score is: {rfr_f1}")
 
 # %% Suport Vector
-sv = SVC(C=10, gamma=0.0001, kernel="linear")
+sv = SVR(C=10, gamma=0.0001, kernel="linear")
 sv.fit(x_train, y_train)
 sv_y_pred = sv.predict(x_test)
-sv_f1 = f1_score(y_test, sv_y_pred, average="weighted")
+sv_f1 = mean_squared_error(y_test, sv_y_pred)
 print(f"Random Forest f1 score is: {sv_f1}")
 
 # %% Cross Validation
-cv = 2
-lr_score = cross_val_score(lr, x, y, scoring="f1_weighted", cv=cv)
-dt_score = cross_val_score(dt, x, y, scoring="f1_weighted", cv=cv)
-rfr_score = cross_val_score(rfr, x, y, scoring="f1_weighted", cv=cv)
-sv_score = cross_val_score(sv, x, y, scoring="f1_weighted", cv=cv)
+cv = 10
+lr_score = cross_val_score(model, x, y, scoring="r2", cv=cv)
+dt_score = cross_val_score(dtr, x, y, scoring="r2", cv=cv)
+rfr_score = cross_val_score(rfr, x, y, scoring="r2", cv=cv)
+sv_score = cross_val_score(sv, x, y, scoring="r2", cv=cv)
 
 # %% Print the scores
 print(
