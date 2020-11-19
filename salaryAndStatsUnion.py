d94a43be244c0c804bde353bd84ee8cb9594c60a
josephcallaghan <callaghan.j@husky.neu.edu>
2020-11-19 11:07:05 -0500

    Remove TODO

--- salaryAndStatsUnion.py
+++ salaryAndStatsUnion.py
@@ -6,5 +6,3 @@ fan_graph_df.drop(columns=["Team", "playerid"], inplace=True)
 df = df.join(fan_graph_df.set_index('Name'), on='Name', lsuffix="_salries", rsuffix="_fan_graphs")
 df.dropna(inplace=True)
 df.to_csv("data/salaries-and-fan-graph-stats.csv")
-
-# TODO: Remove duplicate stats
